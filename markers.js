5540b34bf2dcc74d57c337997b66e386843d5be9
francisco <63866254+ka33al@users.noreply.github.com>
2021-05-15 15:33:15 +0100

    Add files via upload

---
+++ markers.js
@@ -0,0 +1,22 @@
+const markers = [ {
+    "server": "SCUM Server Official #3 - Europe",
+    "left": "22.1%",
+    "top": "43.3%",
+    "loot": "https://steamuserimages-a.akamaihd.net/ugc/1794099926076356549/E336083F2D6257C3D7BDBD6D6B8342D1D04C0B1C/",
+    "colour": "yellow",
+    "createdDate": "2021-05-14T18:35:00.000Z"
+}, {
+    "server": "SCUM Server Official #3 - Europe",
+    "left": "99%",
+    "top": "28%",
+    "loot": "https://steamuserimages-a.akamaihd.net/ugc/1794099926076356549/E336083F2D6257C3D7BDBD6D6B8342D1D04C0B1C/",
+    "colour": "yellow",
+    "createdDate": "2021-05-13T13:35:00.000Z"
+}, {
+    "server": "SCUM Server Official #3 - Europe",
+    "left": "58.43%",
+    "top": "37.56%",
+    "loot": "https://steamuserimages-a.akamaihd.net/ugc/1794100072124467014/9DC92170AA2234E701FCAD0CBCDA7042B4481192/",
+    "colour": "yellow",
+    "createdDate": "2021-05-14T22:00:00.000Z"
+} ];
